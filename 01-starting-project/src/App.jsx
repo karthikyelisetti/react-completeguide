e06312c394a50e058209a9ff2bfcc74e5534d412
karthik.yelisetti <karthikyelisetti.dev@gmail.com>
2025-06-02 07:36:58 +0530

    updating the content randomly and dynamically

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,13 @@
+
+// Defining a array of strings to dynamically update the content.
+const reactDescription = ['Fundamental', 'Crucial', 'Core'];
+function  genRandomInt(max) {
+  // This function generates a random integer between 0 and max (exclusive).
+  return Math.floor(Math.random() * (max + 1));
+}
+
+const description = reactDescription[genRandomInt(2)];
+
 // This is a React component that serves as the main entry point for the application.
 function Header() {
   return (
@@ -5,7 +15,7 @@ function Header() {
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
