355f3b1169d2e1722942185abfc1fcf9ef4c0c67
karthik.yelisetti <karthikyelisetti.dev@gmail.com>
2025-06-16 08:52:29 +0530

    Added new section examples

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,23 +2,24 @@
 import Header from './components/Header/Header.jsx'; // Importing the Header component from the components directory.
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx'; // Importing the CoreConcept component from the components directory.
 import { CORE_CONCEPTS } from './data.js'; // Importing an array of core concepts from a separate data file.
+import TabButton from './components/TabButton/TabButton.jsx'; // Importing the TabButton component from the components directory.
 
 
-function CoreConceptOD({image, title, description}) { // This is an alternative way to define the CoreConcept component using object destructuring.
-  // This allows us to directly access the properties of the props object.
-  // It is a more concise way to write the component, especially when you want to use the properties directly.
-  // However, it is less flexible than using the props object directly, as you cannot pass additional properties that are not defined in the destructured object.
-  // This is useful when you want to ensure that the component only receives specific properties.
-  // It is a common pattern in React to use destructuring to make the code cleaner and more readable.
-  // It is especially useful when you have a lot of properties to pass to the component.
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+// function CoreConceptOD({image, title, description}) { // This is an alternative way to define the CoreConcept component using object destructuring.
+//   // This allows us to directly access the properties of the props object.
+//   // It is a more concise way to write the component, especially when you want to use the properties directly.
+//   // However, it is less flexible than using the props object directly, as you cannot pass additional properties that are not defined in the destructured object.
+//   // This is useful when you want to ensure that the component only receives specific properties.
+//   // It is a common pattern in React to use destructuring to make the code cleaner and more readable.
+//   // It is especially useful when you have a lot of properties to pass to the component.
+//   return (
+//     <li>
+//       <img src={image} alt={title} />
+//       <h3>{title}</h3>
+//       <p>{description}</p>
+//     </li>
+//   );
+// }
 
 
 function App() {
@@ -34,6 +35,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Component</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
