a12b7c70be7a098af928a44395bd2b59346b7a83
karthik.yelisetti <karthikyelisetti.dev@gmail.com>
2025-06-28 08:59:34 +0530

    restructuring the components and code as per coding standards

---
+++ 01-starting-project-deep-dive/src/components/CoreConcept/CoreConcepts.jsx
@@ -0,0 +1,45 @@
+import { CORE_CONCEPTS } from "../../data.js"; // Importing an array of core concepts from a separate data file.
+import CoreConcept from "./CoreConcept.jsx"; // Importing the CoreConcept component from the components directory.
+
+
+// function CoreConceptOD({image, title, description}) { // This is an alternative way to define the CoreConcept component using object destructuring.
+//   // This allows us to directly access the properties of the props object.
+//   // It is a more concise way to write the component, especially when you want to use the properties directly.
+//   // However, it is less flexible than using the props object directly, as you cannot pass additional properties that are not defined in the destructured object.
+//   // This is useful when you want to ensure that the component only receives specific properties.
+//   // It is a common pattern in React to use destructuring to make the code cleaner and more readable.
+//   // It is especially useful when you have a lot of properties to pass to the component.
+//   return (
+//     <li>
+//       <img src={image} alt={title} />
+//       <h3>{title}</h3>
+//       <p>{description}</p>
+//     </li>
+//   );
+// }
+
+function CoreConcepts(){
+    return (
+        <>
+            <section id="core-concepts">
+                <h2>Core Concepts</h2>
+                <ul>
+                {CORE_CONCEPTS.map((conceptItem) => (
+                    <CoreConcept key={conceptItem.title} {...conceptItem} />
+                ))}
+                {/* <CoreConcept {...CORE_CONCEPTS[0]} />{" "}
+                {Using spread operator to pass all properties of the first concept when all the key names are similar}
+                <CoreConcept {...CORE_CONCEPTS[1]} />
+                <CoreConcept {...CORE_CONCEPTS[2]} />
+                <CoreConcept
+                    title={CORE_CONCEPTS[3].title}
+                    description={CORE_CONCEPTS[3].description}
+                    image={CORE_CONCEPTS[3].image}
+                /> */}
+                </ul>
+            </section>
+        </>
+    )
+}
+
+export default CoreConcepts; // Exporting the CoreConcepts component to be used in other parts of the application.
---
+++ 01-starting-project-deep-dive/src/components/Examples/Examples.jsx
@@ -0,0 +1,68 @@
+
+import { EXAMPLES } from "../../data.js"; // Importing an object containing example data from a separate data file.
+import TabButton from "../TabButton/TabButton.jsx"; // Importing the TabButton component from the components directory.
+import { useState } from "react"; // Importing useState hook from React to manage state in functional components.
+
+function Examples() {
+
+    // Using useState to manage the state of the selected tab. The initial state is set to "Component".
+  const [selectedContent, setSelectedContent] = useState(); // Initial state is a string prompting the user to select a topic.
+
+  function handleSelect(selectedTab) {
+    // This function will handle the selection of a tab button.
+    setSelectedContent(selectedTab); // Update the state with the selected tab.
+    // console.log(selectedContent);
+  }
+    return (
+        <>
+            <section id="examples">
+                <h2>Examples</h2>
+                <menu>
+                    <TabButton
+                    isSelected={selectedContent === "components"}
+                    onSelect={() => {
+                        handleSelect("components");
+                    }}
+                    >
+                    Component
+                    </TabButton>
+                    <TabButton
+                    isSelected={selectedContent === "jsx"}
+                    onSelect={() => {
+                        handleSelect("jsx");
+                    }}
+                    >
+                    JSX
+                    </TabButton>
+                    <TabButton
+                    isSelected={selectedContent === "props"}
+                    onSelect={() => {
+                        handleSelect("props");
+                    }}
+                    >
+                    Props
+                    </TabButton>
+                    <TabButton
+                    isSelected={selectedContent === "state"}
+                    onSelect={() => {
+                        handleSelect("state");
+                    }}
+                    >
+                    State
+                    </TabButton>
+                </menu>
+                {!selectedContent ? (
+                    <p>Please select a topic.</p>
+                ) : (
+                    <div id="tab-content">
+                    <h3>{EXAMPLES[selectedContent].title}</h3>
+                    <p>{EXAMPLES[selectedContent].description}</p>
+                    <code>{EXAMPLES[selectedContent].code}</code>
+                    </div>
+                )}
+            </section>
+        </>
+    )
+}
+
+export default Examples; // Exporting the Examples component to be used in other parts of the application.
