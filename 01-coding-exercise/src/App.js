9c4642f888a362d632c81c95d3e6d138226f7d52
karthik.yelisetti <karthikyelisetti.dev@gmail.com>
2025-07-02 10:30:26 +0530

    coding exercise - setting default prop values

---
+++ 01-coding-exercise/src/App.js
@@ -0,0 +1,55 @@
+import Button from './Button';
+import HomeIcon from './HomeIcon';
+import PlusIcon from './PlusIcon';
+
+function App() {
+  return (
+     <div id="app">
+      <section>
+        <h2>Filled Button (Default)</h2>
+        <p>
+          <Button>Default</Button>
+        </p>
+        <p>
+          <Button mode="filled-button">Filled (Default)</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Button with Outline</h2>
+        <p>
+          <Button mode="outline-button">Outline</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Text-only Button</h2>
+        <p>
+          <Button mode="text-button">Text</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Button with Icon</h2>
+        <p>
+          <Button Icon={HomeIcon}>Home</Button>
+        </p>
+        <p>
+          <Button Icon={PlusIcon} mode="text-button">
+            Add
+          </Button>
+        </p>
+      </section>
+      <section>
+        <h2>Buttons Should Support Any Props</h2>
+        <p>
+          <Button mode="filled-button" disabled>
+            Disabled
+          </Button>
+        </p>
+        <p>
+          <Button onClick={() => console.log('Clicked!')}>Click me</Button>
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-coding-exercise/src/Button.js
@@ -0,0 +1,13 @@
+export default function Button({children, mode, Icon}) {
+ // Todo: Build this component!
+ 
+ // !!! Important: 
+ // Wrap the icon with a <span className="button-icon"> to achieve the target look
+ // Also wrap the children prop with a <span>
+    return (
+        <>
+            {/* <span className="button-icon">{Icon}</span> */}
+            <span className={mode ? mode : "filled-button"}>{children}</span>
+        </>
+    );
+}
